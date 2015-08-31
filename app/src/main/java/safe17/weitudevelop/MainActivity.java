f01639c68de4f9923b6ddb12e87e51287b33b0f0
ouxuewen <ouxuewen@360.cn>
2015-08-31 12:43:31 +0800

    add topbar

--- app/src/main/java/safe17/weitudevelop/MainActivity.java
+++ app/src/main/java/safe17/weitudevelop/MainActivity.java
@@ -5,17 +5,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
+//import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private TextView myTextView;
     EditText password1;
     EditText password2;
