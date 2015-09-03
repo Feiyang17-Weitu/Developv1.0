61f754ec3315cb3c9c9404ee81cbd198045c8ba4
mig_davidli <mig_davidli@163.com>
2015-09-03 17:05:00 +0800

    Add fake password logic.

---
+++ app/src/main/java/safe17/weitudevelop/PasswdInfo.java
@@ -0,0 +1,10 @@
+package safe17.weitudevelop;
+
+import android.content.SharedPreferences;
+
+/**
+ * Created by lidecai on 2015/9/3.
+ */
+public class PasswdInfo {
+    public static SharedPreferences preferences;
+}
