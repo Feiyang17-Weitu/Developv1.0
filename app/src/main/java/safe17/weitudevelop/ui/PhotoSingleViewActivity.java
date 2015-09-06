7ccb6779f4cec72d0dda706a199607798881892e
wangxubing <wangxubing@360.cn>
2015-09-06 22:15:05 +0800

    show picture

--- app/src/main/java/safe17/weitudevelop/ui/PhotoSingleViewActivity.java
+++ app/src/main/java/safe17/weitudevelop/ui/PhotoSingleViewActivity.java
@@ -9,7 +9,9 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -17,6 +19,8 @@ import java.util.List;
 
 import safe17.weitudevelop.R;
 import safe17.weitudevelop.tool.BitmapUtils;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 /**
  * Created by ouxuewen on 2015/9/5.
@@ -27,6 +31,12 @@ public class PhotoSingleViewActivity extends Activity implements ViewPager.OnPag
 
     private List<ImageView> imageIdList;
     private int first_position;
+    private Button btnDelPicture = null;
+    private Button btnTransferPicture = null;
+    private TextView picturePosition = null;
+    private Button btnBack = null;
+    private ArrayList<String> position_array = new ArrayList<String>();
+    private boolean boFullScreen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,33 +44,102 @@ public class PhotoSingleViewActivity extends Activity implements ViewPager.OnPag
 
         setContentView(R.layout.photo_single_view);
         mViewPager = (ViewPager)findViewById(R.id.vf_photo);
+
         imageIdList = new ArrayList<ImageView>();
+        btnDelPicture = (Button)findViewById(R.id.del_picture);
+        btnTransferPicture = (Button)findViewById(R.id.tansfer_picture);
 
         Intent it = super.getIntent();
         first_position = it.getIntExtra("position", 1);
-        ArrayList<String> position_array = it.getStringArrayListExtra("position_array");
+        position_array = it.getStringArrayListExtra("position_array");
 
         for (int i=0;i<position_array.size();i++){
             imageIdList.add(getBitmap(position_array.get(i)));
         }
 
+        picturePosition = (TextView)findViewById(R.id.picture_name);
+        String strPicturePosition = String.valueOf(first_position) + "/" + String.valueOf(position_array.size());
+        picturePosition.setText(strPicturePosition);
+
         mViewPager.setAdapter(new MyAdapter());
         mViewPager.setOnPageChangeListener(this);
+
+        btnBack = (Button)findViewById(R.id.back);
+        btnBack.setOnClickListener(new GoBackClickListener());
+
+        btnDelPicture = (Button)findViewById(R.id.del_picture);
+        btnDelPicture.setOnClickListener(new DelPictureClickListener());
     }
 
     private ImageView getBitmap(String fileName) {
 
         ImageView imageView  = new ImageView(this);
         imageView.setImageBitmap(BitmapUtils.decodeSampledBitmapFromFd(fileName, 500));
+
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(PhotoSingleViewActivity.this,"点击了" + mViewPager.getCurrentItem(),Toast.LENGTH_SHORT).show();
+                if (!boFullScreen)
+                {
+                    btnBack.setVisibility(View.INVISIBLE);
+                    picturePosition.setVisibility(View.INVISIBLE);
+                    btnTransferPicture.setVisibility(View.INVISIBLE);
+                    btnDelPicture.setVisibility(View.INVISIBLE);
+
+                    boFullScreen = true;
+                }
+                else
+                {
+                    btnBack.setVisibility(View.VISIBLE);
+                    picturePosition.setVisibility(View.VISIBLE);
+                    btnTransferPicture.setVisibility(View.VISIBLE);
+                    btnDelPicture.setVisibility(View.VISIBLE);
+                    boFullScreen = false;
+                }
+
+//                Toast.makeText(PhotoSingleViewActivity.this,"点击了" + mViewPager.getCurrentItem(),Toast.LENGTH_SHORT).show();
             }
         });
         return imageView;
     }
 
+    class GoBackClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            PhotoSingleViewActivity.this.finish();
+        }
+    }
+
+    class DelPictureClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(PhotoSingleViewActivity.this);
+            //    设置Title的图标
+            builder.setIcon(R.mipmap.ic_launcher);
+            //    设置Title的内容
+            builder.setTitle("弹出警告框");
+            //    设置Content来显示一个信息
+            builder.setMessage("确定删除吗？");
+            //    设置一个PositiveButton
+            builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(PhotoSingleViewActivity.this, "positive: " + which, Toast.LENGTH_SHORT).show();
+                }
+            });
+            //    设置一个NegativeButton
+            builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(PhotoSingleViewActivity.this, "negative: " + which, Toast.LENGTH_SHORT).show();
+                }
+            });
+            //    显示出该对话框
+            builder.show();
+        }
+    }
 
     @Override
     public void onPageScrollStateChanged(int arg0) {
@@ -81,7 +160,6 @@ public class PhotoSingleViewActivity extends Activity implements ViewPager.OnPag
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
         return super.onTouchEvent(event);
     }
 
